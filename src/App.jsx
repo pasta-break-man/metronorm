a3629713e3ac79db53610183be353fadaf0d9975
Osushi Yasushi <girohasami103@gmail.com>
2026-02-08 01:09:36 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BackCanvas from './compornents/canvas'
+
+function App() {
+  return (
+    <div>
+      <BackCanvas />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/canvas.jsx
@@ -0,0 +1,10 @@
+import react, {useEffect, useRef} from "react";
+import './components.css'
+
+export default function BackCanvas(){
+    return(
+        <div className="canvas">
+            <div className="gamen"></div>
+        </div>
+    )
+}
