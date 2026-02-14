362f000526d0d5606576c5cabc52028c2827dae6
Osushi Yasushi <girohasami103@gmail.com>
2026-02-15 01:53:59 +0900

    5回目．メトロノームの音声がなるようになった

--- src/components/Bottons_UpDown.jsx
+++ src/components/Bottons_UpDown.jsx
@@ -1,8 +1,10 @@
-import React, {Component} from "react";
+import React, {Component, useEffect} from "react";
 import './components.css';
+import Sound from './beat_440.wav';
+import useSound from 'use-sound';
 
 class Toggle extends Component{
-    state = {speed: 0, onoff: true};
+    state = {speed: 60, onoff: false};
 
     handleUp = () => {
         this.setState(prev => ({
@@ -29,9 +31,29 @@ class Toggle extends Component{
                 <DownToggle onDown={this.handleDown}/>
                 <GamenPrevew speed={this.state.speed}/>
                 <On_Off_swich swich={this.Getonoff} onoff={this.state.onoff}/>
+                <Roop_wav isOn={this.state.onoff} speed={this.state.speed}/>
             </> //ラップしないことにより，親子関係を正しくできるっぽい
         );
     }
+}
+
+function Roop_wav({isOn, speed}){
+    const [play, {stop}] = useSound(Sound, {interrupt: true});
+    useEffect(() => {
+        if(!isOn || speed <= 0){
+            stop?.();
+            return;
+        }
+
+        const bpm = 60000 / speed;
+        const id = setInterval(() => {
+            play();
+        }, bpm);
+
+        return() => clearInterval(id);
+    }, [isOn, speed, play, stop]);
+    
+    return null;
 }
 
 export function UpToggle(props){
@@ -62,6 +84,7 @@ export function On_Off_swich({swich, onoff}){
     return(
         <div className='playmetro'>
             <button onClick={swich}>{onoff ? 'ON': 'OFF'}</button>
+            <Roop_wav/>
         </div>
     )
 }
