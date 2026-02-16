79b0f9cd1f04956ec72055e3057e883217325816
Osushi Yasushi <girohasami103@gmail.com>
2026-02-17 02:18:13 +0900

    6回目．ポップアップ表示でBPM を設定できるようにしたが，ちょっと微妙．ポップアップのやり方については学びながら直すとして，CSSは早く適応しておきたいところ

--- src/components/Bottons_UpDown.jsx
+++ src/components/Bottons_UpDown.jsx
@@ -1,10 +1,10 @@
-import React, {Component, useEffect} from "react";
+import React, {Component, useEffect, useState} from "react";
 import './components.css';
 import Sound from './beat_440.wav';
 import useSound from 'use-sound';
 
 class Toggle extends Component{
-    state = {speed: 60, onoff: false};
+    state = {speed: 60, onoff: false, text: "", setText: ""};
 
     handleUp = () => {
         this.setState(prev => ({
@@ -29,7 +29,7 @@ class Toggle extends Component{
             <>
                 <UpToggle onUp={this.handleUp}/>
                 <DownToggle onDown={this.handleDown}/>
-                <GamenPrevew speed={this.state.speed}/>
+                <GamenPrevew speed={this.state.speed} onspeedChange={(n) => this.setState({speed: n})}/>
                 <On_Off_swich swich={this.Getonoff} onoff={this.state.onoff}/>
                 <Roop_wav isOn={this.state.onoff} speed={this.state.speed}/>
             </> //ラップしないことにより，親子関係を正しくできるっぽい
@@ -56,6 +56,30 @@ function Roop_wav({isOn, speed}){
     return null;
 }
 
+function Text_in({speed, onspeedChange}){
+    const [text, setText] = useState("");
+
+    const handleChange = (e) => {
+        const set = e.target.value;
+        setText(set);
+
+        const set_number = Number(set);
+        onspeedChange(set_number);
+    }
+
+    return(
+        <div>
+            <input
+            type="text"
+            value={text}
+            onChange={handleChange}
+            placeholder="数字を入力して"
+            />
+            <p>BPM: {text}</p>
+        </div>
+    )
+}
+
 export function UpToggle(props){
     return(
         <div className='playsrightnormal'>
@@ -72,10 +96,24 @@ export function DownToggle(props){
     )
 }
 
-export function GamenPrevew(props){
+function GamenPrevew({speed, onspeedChange}){
+    const [isPopUpVisible, setPopUpVisible] = useState(false);
+
+    const togglePopUp = () => {
+        setPopUpVisible(!isPopUpVisible);
+    };
+
     return(
         <div className='gamen'>
-            <h1 speed={props.speed}>speed: {props.speed}</h1>
+            <button onClick={togglePopUp}></button>
+
+            {isPopUpVisible && (
+                <div className="Popup">
+                    <Text_in speed={speed} onspeedChange={onspeedChange}/>
+                    <button onClick={togglePopUp}>close</button>
+                </div>
+            )}
+            <h1>speed: {speed}</h1>
         </div>
     )
 }
@@ -84,7 +122,6 @@ export function On_Off_swich({swich, onoff}){
     return(
         <div className='playmetro'>
             <button onClick={swich}>{onoff ? 'ON': 'OFF'}</button>
-            <Roop_wav/>
         </div>
     )
 }
